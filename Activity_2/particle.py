ff33184bab09749d948811df3a0a97de36796ae4
leonardo Neia <leonardo.thomaz@ufu.br>
2025-04-18 14:40:34 -0300

    Add activity 2

---
+++ Activity_2/particle.py
@@ -0,0 +1,20 @@
+# particle.py
+
+class Particle:
+
+    def __init__(self, x, y, vx, vy, mass):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.mass = mass
+
+    def newton(self, fx, fy, dt):
+        ax = fx / self.mass
+        ay = fy / self.mass
+        self.vx += ax * dt
+        self.vy += ay * dt
+        self.x += self.vx * dt
+        self.y += self.vy * dt
+
+
